d42d6480f9928d075436063bbcc29a03e6f7a41d
aybukeberfinsuer <aybukeberfinsuer@gmail.com>
2024-12-27 03:21:44 +0300

    updates

---
+++ YummyProject/Controllers/FeatureController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YummyProject.Context;
+using YummyProject.Models;
+
+namespace YummyProject.Controllers
+{
+    public class FeatureController : Controller
+    {
+        YummyContext _context= new YummyContext();
+        public ActionResult Index()
+        {
+           var values= _context.Features.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddFeature()
+        {
+            return View();
+        }
+		[HttpPost]
+		public ActionResult AddFeature(Feature feature)
+		{
+           _context.Features.Add(feature);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+		}
+
+        public ActionResult RemoveFeature(int id)
+        {
+
+            var value = _context.Features.Find(id);
+            _context.Features.Remove(value);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+		}
+	}
+}
